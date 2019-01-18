834f46770632aebb7a09ae7552775d41aaac2d61
TiagoPortfolio <tiagosousa28@hotmail.com>
2019-01-18 16:28:20 +0000

    Add fade in to event

--- events-listing/src/components/Event.js
+++ events-listing/src/components/Event.js
@@ -1,27 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
 import SubscriptionsList from './SubscriptionsList';
 
-const Event = ({event}) => {
-  const categories = event.categories
-    .map((category, index) => <span key={index}>{category}</span>)
-    .reduce((prev, curr) => [prev, ' / ', curr]);
+class Event extends Component {
+  constructor(props) {
+    super(props);
 
-  return (
-    <div className="event-item">
-      <div className="box-info">
-        <div className="box-info--content">
-          <div className="description">
-            <h1>{event.name}</h1>
-            <p>{event.description}</p>
+    this.state = {
+      opacity: 0,
+    }
+  }
+
+  componentDidMount() {
+    setTimeout(() =>
+      this.setState({ opacity: 1})
+    , 100);
+  }
+  
+  render() {
+    const categories = this.props.event.categories
+      .map((category, index) => <span key={index}>{category}</span>)
+      .reduce((prev, curr) => [prev, ' / ', curr]);
+
+    return (
+      <div className="event-item" style={{...this.state}}>
+        <div className="box-info">
+          <div className="box-info--content">
+            <div className="description">
+              <h1>{this.props.event.name}</h1>
+              <p>{this.props.event.description}</p>
+            </div>
+            <div className="tags">{categories}</div>
           </div>
-          <div className="tags">{categories}</div>
-        </div>
-        <div className="box-info--footer">
-          <SubscriptionsList subscriptions={event.subscriptions} />
+          <div className="box-info--footer">
+            <SubscriptionsList subscriptions={this.props.event.subscriptions} />
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  };
 };
 
 export default Event;
